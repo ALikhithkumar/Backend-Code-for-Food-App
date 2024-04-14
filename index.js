86404f83a1a51c300dff0e210f5f046b40f5afdb
Likhith <alikhith123@gmail.com>
2024-04-14 16:24:34 +0530

    modified index file for universal port

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const firmRoutes = require('./routes/firmRoutes')
 const productRoutes = require('./routes/productRoutes')
 const app = express()
 
-const path=require('path')
+const path = require('path')
+const PORT = process.env.PORT || 4000
 dotEnv.config()
 console.log("Hi")
 mongoose.connect(process.env.MONGO_URL)
@@ -17,8 +18,10 @@ app.use(bodyParser.json())
 app.use('/vendor', vendorRoutes)
 app.use('/firm', firmRoutes)
 app.use('/product', productRoutes)
-app.use('/uploads',express.static('/uploads'))
-app.listen(4000, () => {
+app.use('/uploads', express.static('/uploads'))
+app.listen(PORT, () => {
     console.log("server listening ")
 })
-
+app.use('/', (req, res) => {
+    res.sendFile("<h1>Welcome")
+})
